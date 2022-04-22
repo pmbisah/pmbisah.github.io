79cbdf69f6387c889e0b3cdcfba3d33c7019a350
pmbisah <petermbisah@gmail.com>
2022-04-22 13:55:16 +0200

    Debugged

--- python/src/unpackaged/abm/agentframework.py
+++ python/src/unpackaged/abm/agentframework.py
@@ -9,10 +9,10 @@ Created by 201566344
 15th April 2022'''
 import random 
 
-class Agent: # Creating class
+class Agent(): # Creating class
 
     def __init__ (self, environment, agents):  
-        self.y = random.randint(0,99)
+        self.y = random.randint(0,99)   
         self.x = random.randint(0,99)     
         self.environment = environment  #setting environment
         self.agents = agents            #setting agents
@@ -34,4 +34,18 @@ class Agent: # Creating class
         if  self.environment[self.y][self.x] > 10:
             self.environment[self.y][self.x] -= 10
             self.store += 10 
- 
+   # Measure distance between ageants
+    def distance_between(self, agent):
+            return (((self.x- agent.x)**2) +
+            ((self.y - agent.y)**2))**0.5
+        
+    #share  with close neighbours
+    def share_with_neighbours(self, neighbourhood):
+            for agent in self.agents:
+                distance = self.distance_between(agent)
+                if distance  <= neighbourhood:
+                    sum = self.store + agent.store
+                    average = sum /2
+                    self.store = average
+                    agent.store = average
+                    print("shares " + str(distance) + " " + str(average))
--- python/src/unpackaged/abm/model.py
+++ python/src/unpackaged/abm/model.py
@@ -7,27 +7,22 @@ Created by 201566344
 
 5th April 2022"""
 
-import random # this random number generator will be used in step 3
+import random # this random number will be used for the agentframework module
 import operator # this is required to calculate distance between agents later
+import matplotlib
+matplotlib.use('TkAgg') 
 import matplotlib.pyplot # this is used to plot agents on a graph
-import matplotlib.animation
+import matplotlib.animation 
 import agentframework  # this imports tahe agentframwork.py class from the same directory as the model.py
 import csv  # this is used to import csv file to read an external txt file
 
 
-# function for distance calculation between agent to be used later in a loop
-def distance_between(agents_row_a, agents_row_b):
-    return (((agents_row_a.x- agents_row_b.x)**2) + ((agents_row_a.y - agents_row_b.y)**2))**0.5
-
-"""
-print(distance_between)
-"""
-# Set Parameters
-num_of_agents = input('Enter Number of Agents-Interger') 
-num_of_iterations =100
-neighbourhood = 20 
-environment = []
-agents = []
+# SET PARAMETERS THE PARAMETERS CAN BE CHANGE HERE
+num_of_agents = 10      #Number of Agents
+num_of_iterations =100  #Number of Iterations
+neighbourhood = 20      #Numer of Neighborhoods
+environment = []        # Crates an empty list 
+agents = []             # Crates an empty list 
 
 
 """
@@ -37,43 +32,40 @@ print(a.y, a.x)
 a.move()
 print(a.y, a.x)
 """
-
-f = open('in.txt', newline='')
-reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-for row in reader: # A list of rows
-    for value in row: # A list of value
-        print(value) # Floats
-
-
+# Reading a text file and placing it into an enviroment              
+f = open('in.txt', newline='')      #read the text a line at a time
+reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC) #convert to float
 for row in reader:				     #reading in the rows a row at a time
     rowlist = []                    
     for value in row:
         rowlist.append(value) 
-    environment.append(rowlist)  
+    environment.append(rowlist)       # placing it into the evoronment
 f.close()
-
 
 
 # Make agents based on num_agents variable in relation to the environment
 for i in range(num_of_agents):
     agents.append(agentframework.Agent(environment, agents))
 
-
 """
 matplotlib.pyplot.imshow(environment)
 matplotlib.pyplot.show()
-"""
 
-     
-for j in range(num_of_iterations):
-    for i in range(num_of_agents):
-        agents[i].move()
-        agents[i].eat()
-        
+"""
+# Ploting the agents in the envronment         
 matplotlib.pyplot.xlim(0, 99)
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.imshow(environment)
 for i in range(num_of_agents):
     matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
 matplotlib.pyplot.show()
+
+#  a loop for moving the agent eat and sharing points with neighbours
+
+for j in range(num_of_iterations):
+    for i in range(num_of_agents):
+        agents[i].move()
+        agents[i].eat()
+        agents[i].share_with_neighbours(neighbourhood)
+
 
